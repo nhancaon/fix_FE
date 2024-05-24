76f1d836dd3367bed58c671b716b837102e6f4bb
sage0304 <hiennhanlonggiang@gmail.com>
2024-05-24 18:43:26 +0700

    fix UI BOM

--- app/(productManager)/PMBOM.jsx
+++ app/(productManager)/PMBOM.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-import IconButton from '../../components/IconButton';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { getAllBOMs, getBOMlikeName, getBOMsByStatus } from '../../services/BOMServices';
 import { useGlobalContext } from '../../context/GlobalProvider';
@@ -9,8 +8,8 @@ import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { CustomButton } from '../../components';
 
 const PMBOM = () => {
-  const { token, searchText, setSearchText  } = useGlobalContext();
-  const [search, setSearch] = useState('');
+  const navigation = useNavigation();
+  const { token, searchText } = useGlobalContext();
   const [boms, setBoms] = useState([]);
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
@@ -19,7 +18,7 @@ const PMBOM = () => {
     { label: 'Check Price', value: 'CHECK_PRICE' },
     { label: 'Finish', value: 'FINISH' },
   ]);
- 
+
   const getFilteredData = () => {
     if (searchText.trim() === '') {
       return boms;
@@ -37,24 +36,9 @@ const PMBOM = () => {
         const data = await getAllBOMs(token);
         setBoms(data.result);
       };
-  
       fetchData();
     }, [token])
   );
-
-  const handleSearch = () => {
-    console.log(`Search for ${search}`);
-    console.log(`token ${token}`)
-    try {
-      const fetchData = async () => {
-        const data = await getBOMlikeName(token, search);
-        setBoms(data.result);
-      };
-      fetchData();
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const handleInsert = () => {
     try {
@@ -64,16 +48,6 @@ const PMBOM = () => {
     }
   };
 
-  const handleUpdate = () => {
-    // Implement update functionality here
-  };
-
-  const handleDelete = () => {
-    // Implement delete functionality here
-  };
-
-  const navigation = useNavigation();
-  
   const handleCardPress = async (id) => {
     console.log(`Card with id ${id} was pressed.`);
     try {
@@ -84,53 +58,51 @@ const PMBOM = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <View className="bg-primary h-full" style={{ flex: 1 }}>
       <DropDownPicker
-              open={open}
-              value={value}
-              items={items}
-              setOpen={setOpen}
-              setValue={setValue}
-              setItems={setItems}
-              containerStyle={{ height: 40, width: '35%', alignSelf: 'flex-end' }}
-              style={{ backgroundColor: '#fafafa' }}
-              itemStyle={{ justifyContent: 'flex-start' }}
-              dropDownStyle={{ backgroundColor: '#fafafa' }}
-              onChangeValue={async (value) => {
-                console.log('onChangeValue called with:', value);
-                setValue(value);
-                try {
-                  const data = await getBOMsByStatus(token, value);
-                  if (data) {
-                    console.log('data:', data.result);
-                    setBoms(data.result);
-                  }
-                } catch (error) {
-                  console.error(error);
-                }
-              }}
-            />
-      {/* get All */}
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        containerStyle={[styles.dropDownContainer, open && { zIndex: 1000 }]}
+        style={styles.dropDown}
+        itemStyle={{ justifyContent: 'flex-start' }}
+        dropDownStyle={styles.dropDown}
+        onChangeValue={async (value) => {
+          console.log('onChangeValue called with:', value);
+          setValue(value);
+          try {
+            const data = await getBOMsByStatus(token, value);
+            if (data) {
+              console.log('data:', data.result);
+              setBoms(data.result);
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        }}
+      />
       <FlatList
         data={filteredData}
         keyExtractor={item => item.id.toString()}
         renderItem={({ item }) => (
           <Card style={styles.card} onPress={() => handleCardPress(item.id)}>
-            <Card.Title title={item.bomname} titleStyle={styles.title}/>
-            <Card.Content>
-              <Text>Total Price: {item.totalPrice}</Text>
-              <Text>Sell Price: {item.sellPrice}</Text>
-              <Text>Date Creation: {item.dateCreation}</Text>
+            <Card.Title title={item.bomname} titleStyle={styles.title} />
+            <Card.Content style={styles.cardContent}>
+              <Text style={styles.text}>Total Price: {item.totalPrice}</Text>
+              <Text style={styles.text}>Sell Price: {item.sellPrice}</Text>
+              <Text style={styles.text}>Date Creation: {item.dateCreation}</Text>
             </Card.Content>
           </Card>
         )}
       />
-
-      <View>  
+      <View>
         <CustomButton
           icon={"plus"}
           iconSize={28}
-          containerStyles="p-0 absolute bottom-4 self-end right-4 h-12 w-12 rounded-full bg-green-500 items-center justify-center"
+          containerStyles="p-0 absolute bottom-12 self-end right-4 h-12 w-12 rounded-full bg-green-500 items-center justify-center"
           isLoading={false}
           handlePress={handleInsert}
         />
@@ -140,27 +112,34 @@ const PMBOM = () => {
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    position: 'absolute',
-    bottom: 15,
-    width: '100%',
-
+  dropDownContainer: {
+    height: 40,
+    width: '37%',
+    alignSelf: 'flex-end',
+    zIndex: 2000,
+    marginEnd: 10,
+    marginVertical: 5,
+  },
+  dropDown: {
+    backgroundColor: '#FFA500',
   },
   card: {
     margin: 10,
     padding: 10,
   },
-  material: {
-    marginTop: 10,
-  },
   title: {
-    color: '#FFA500', // Màu cam
-    fontSize: 20, // Kích thước font
-    fontWeight: 'bold', // Đặt font chữ đậm
+    color: '#FFA500', // Orange color
+    fontSize: 20, // Font size
+    fontWeight: 'bold', // Bold font
   },
-
+  cardContent: {
+    
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
 });
+
 
 export default PMBOM;
